c48d834db4f1a9071b7c3cb9eb3d5df60d62187b
Alin Eugen Deac <aedart@gmail.com>
2016-11-13 14:11:10 +0100

    Add TimerMaster (incomplete)

---
+++ src/TimerMaster.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * TimerMaster
+ *
+ * @description Handles various timers
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class TimerMaster {
+
+    constructor(){
+
+    }
+    //
+    // addTimeout(id, callback, delay){
+    //
+    // }
+    //
+    // addInterval(id, callback, delay){
+    //
+    // }
+    //
+    // // set(id, ){
+    // //
+    // // }
+    //
+    // get(id){
+    //
+    // }
+    //
+    // cancel(id){
+    //
+    // }
+    //
+    // clear(){
+    //
+    // }
+}
+
+export default TimerMaster;
